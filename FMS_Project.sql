df42ea6a99ace348fe8f63dce178210fc95500ff
haribalakrishnan.v@northeastern.edu <haribalakrishnan.v@northeastern.edu>
2021-04-12 00:08:24 -0400

    Table Creation Commited

--- FMS_Project.sql
+++ FMS_Project.sql
@@ -2,3 +2,6 @@ set serveroutput on;
 begin
 DBMS_OUTPUT.PUT_LINE('DMDD Project');
 end;
+drop table fisherman_details;
+select * from fisherman_details;
+Insert into fish_species values(514,'Yellow Perch',112,20);
