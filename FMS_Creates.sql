2a4e74e1499ee616a08dbd7fc7d5cdf539f96e11
saivigneshs <sridhar.sa@northeastern.edu>
2021-04-26 15:23:50 -0400

    Added Proc to book a slot

--- FMS_Creates.sql
+++ FMS_Creates.sql
@@ -123,7 +123,41 @@ ALTER TABLE SUB_LOCATION
 ADD FOREIGN KEY (loc_id) REFERENCES location(loc_id);
 ---vishal ends
 
+---Saivignesh Start--
+--PROC for booking a slot --
 
+Create or replace procedure Book_slot (pi_fm_id int, pi_slot_id int, po_booking_id out int)
+as 
+slot_ct int := 0;
+BEGIN
+select slot_count into slot_ct from "SLOTS" where slot_id = pi_slot_id;
+if slot_ct <= 0
+then
+dbms_output.Put_line('This Slot is currently full. Please try a different slot.');
+return;
+elsif slot_ct between 1 and 8
+then
+dbms_output.Put_line('Very few slots are available, Hurry up!');
+return;
+else
+SELECT MAX(booking_id)+1 into po_booking_id FROM "BOOKINGS";
+insert into bookings(booking_id, fm_id, slot_id, booking_time, book_status) values(po_booking_id,pi_fm_id,pi_slot_id,sysdate,'SUCCESS');
+dbms_output.Put_line('Booked Slot ' || pi_slot_id || ' Successfully! '|| po_booking_id); 
+update slots set slot_count = slot_ct - 1 where slot_id = pi_slot_id; 
+dbms_output.Put_line('Updated Slots Table for ' || pi_slot_id || ' Successfully! '); 
+end if;
+END;
+
+select * from fisherman where fm_id = '2';
+--2	Dean	Bradley	46	M	3	(774) 350-3917	erat.Vivamus@ac.net
+select * from slots where slot_id = '101';
+--101	Tuesday	01-APR-21 06.00.00.000000000 AM	12	
+select * from bookings where booking_id= '301';	
+declare po_book_id INT := 0;
+BEGIN
+BOOK_SLOT (2,101,po_book_id);
+END;
+--- saivignesh ends
 
 
 
